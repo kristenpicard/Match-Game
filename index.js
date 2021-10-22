6ddb50695d317c385d08dfbdd5806e3cfc99eed7
Kristen Picard <kristenpicard93@gmail.com>
2021-10-22 11:44:40 -0500

    added didWin, shuffled cards

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-"use strict";
-
 var card1 = document.getElementById("card1");
 var card2 = document.getElementById("card2");
 var card3 = document.getElementById("card3");
@@ -9,18 +7,46 @@ var card6 = document.getElementById("card6");
 var card7 = document.getElementById("card7");
 var card8 = document.getElementById("card8");
 
-card1.innerHTML = "&#160;";
-card2.innerHTML = "&#160;";
-card3.innerHTML = "&#160;";
-card4.innerHTML = "&#160;";
-card5.innerHTML = "&#160;";
-card6.innerHTML = "&#160;";
-card7.innerHTML = "&#160;";
-card8.innerHTML = "&#160;";
+card1.innerHTML = "&nbsp;";
+card2.innerHTML = "&nbsp;";
+card3.innerHTML = "&nbsp;";
+card4.innerHTML = "&nbsp;";
+card5.innerHTML = "&nbsp;";
+card6.innerHTML = "&nbsp;";
+card7.innerHTML = "&nbsp;";
+card8.innerHTML = "&nbsp;";
 
 let chosen = { element1: "", element2: "", firstCardID: "" };
 let isElement1Defined = false;
 let isElement2Defined = false;
+
+let numArray = [1, 1, 2, 2, 3, 3, 4, 4];
+
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
+
+let shuffledArray = shuffle(numArray);
+
+function waitInSeconds(func, n) {
+  setTimeout(func, n * 1000);
+}
 
 function checkIfSame(firstCardID, secondCardID) {
   let firstCard;
@@ -64,8 +90,8 @@ function checkIfSame(firstCardID, secondCardID) {
   if (firstCard.innerHTML == secondCard.innerHTML) {
     //do nothing
   } else {
-    firstCard.innerHTML = "&#160;";
-    secondCard.innerHTML = "&#160;";
+    firstCard.innerHTML = "&nbsp;";
+    secondCard.innerHTML = "&nbsp;";
   }
 }
 
@@ -90,96 +116,117 @@ function onClick(e) {
   }
 
   if (e.target.id == "card1") {
-    card1.innerHTML = "1";
+    card1.innerHTML = shuffledArray[0];
+
     if (isElement1Defined == false && isElement2Defined == false) {
-      chosen["element1"] = 1;
+      chosen["element1"] = shuffledArray[0];
       chosen["firstCardID"] = e.target.id;
     } else if (isElement1Defined == true && isElement2Defined == false) {
-      chosen["element2"] = 1;
-      checkIfSame(chosen.firstCardID, e.target.id);
+      chosen["element2"] = shuffledArray[0];
+      waitInSeconds(function () {
+        checkIfSame(chosen.firstCardID, e.target.id);
+      }, 1);
     }
   } else if (e.target.id == "card2") {
-    card2.innerHTML = "2";
+    card2.innerHTML = shuffledArray[1];
+
     if (isElement1Defined == false && isElement2Defined == false) {
       console.log("test");
-      chosen["element1"] = 2;
+      chosen["element1"] = shuffledArray[1];
       chosen["firstCardID"] = e.target.id;
     } else if (isElement1Defined == true && isElement2Defined == false) {
-      chosen["element2"] = 2;
-      checkIfSame(chosen.firstCardID, e.target.id);
+      chosen["element2"] = shuffledArray[1];
+      waitInSeconds(function () {
+        checkIfSame(chosen.firstCardID, e.target.id);
+      }, 1);
     }
   } else if (e.target.id == "card3") {
-    card3.innerHTML = "3";
+    card3.innerHTML = shuffledArray[2];
     if (isElement1Defined == false && isElement2Defined == false) {
-      chosen["element1"] = 3;
+      chosen["element1"] = shuffledArray[2];
       chosen["firstCardID"] = e.target.id;
     } else if (isElement1Defined == true && isElement2Defined == false) {
-      chosen["element2"] = 3;
-      checkIfSame(chosen.firstCardID, e.target.id);
+      chosen["element2"] = shuffledArray[2];
+      waitInSeconds(function () {
+        checkIfSame(chosen.firstCardID, e.target.id);
+      }, 1);
     }
   } else if (e.target.id == "card4") {
-    card4.innerHTML = "4";
+    card4.innerHTML = shuffledArray[3];
     if (isElement1Defined == false && isElement2Defined == false) {
-      chosen["element1"] = 4;
+      chosen["element1"] = shuffledArray[3];
       chosen["firstCardID"] = e.target.id;
     } else if (isElement1Defined == true && isElement2Defined == false) {
-      chosen["element2"] = 4;
-      checkIfSame(chosen.firstCardID, e.target.id);
+      chosen["element2"] = shuffledArray[3];
+      waitInSeconds(function () {
+        checkIfSame(chosen.firstCardID, e.target.id);
+      }, 1);
     }
   } else if (e.target.id == "card5") {
-    card5.innerHTML = "1";
+    card5.innerHTML = shuffledArray[4];
     if (isElement1Defined == false && isElement2Defined == false) {
-      chosen["element1"] = 1;
+      chosen["element1"] = shuffledArray[4];
       chosen["firstCardID"] = e.target.id;
     } else if (isElement1Defined == true && isElement2Defined == false) {
-      chosen["element2"] = 1;
-      checkIfSame(chosen.firstCardID, e.target.id);
+      chosen["element2"] = shuffledArray[4];
+      waitInSeconds(function () {
+        checkIfSame(chosen.firstCardID, e.target.id);
+      }, 1);
     }
   } else if (e.target.id == "card6") {
-    card6.innerHTML = "2";
+    card6.innerHTML = shuffledArray[5];
     if (isElement1Defined == false && isElement2Defined == false) {
-      chosen["element1"] = 2;
+      chosen["element1"] = shuffledArray[5];
       chosen["firstCardID"] = e.target.id;
     } else if (isElement1Defined == true && isElement2Defined == false) {
-      chosen["element2"] = 2;
-      checkIfSame(chosen.firstCardID, e.target.id);
+      chosen["element2"] = shuffledArray[5];
+      waitInSeconds(function () {
+        checkIfSame(chosen.firstCardID, e.target.id);
+      }, 1);
     }
   } else if (e.target.id == "card7") {
-    card7.innerHTML = "3";
+    card7.innerHTML = shuffledArray[6];
     if (isElement1Defined == false && isElement2Defined == false) {
-      chosen["element1"] = 3;
+      chosen["element1"] = shuffledArray[6];
       chosen["firstCardID"] = e.target.id;
     } else if (isElement1Defined == true && isElement2Defined == false) {
-      chosen["element2"] = 3;
-      checkIfSame(chosen.firstCardID, e.target.id);
+      chosen["element2"] = shuffledArray[6];
+      waitInSeconds(function () {
+        checkIfSame(chosen.firstCardID, e.target.id);
+      }, 1);
     }
   } else if (e.target.id == "card8") {
-    card8.innerHTML = "4";
+    card8.innerHTML = shuffledArray[7];
     if (isElement1Defined == false && isElement2Defined == false) {
-      chosen["element1"] = 4;
+      chosen["element1"] = shuffledArray[7];
       chosen["firstCardID"] = e.target.id;
     } else if (isElement1Defined == true && isElement2Defined == false) {
-      chosen["element2"] = 4;
-      checkIfSame(chosen.firstCardID, e.target.id);
+      chosen["element2"] = shuffledArray[7];
+      waitInSeconds(function () {
+        checkIfSame(chosen.firstCardID, e.target.id);
+      }, 1);
     }
   }
-  //   didWin();
+
+  waitInSeconds(function () {
+    didWin(card1, card2, card3, card4, card5, card6, card7, card8);
+  }, 1);
 }
 
-// function didWin() {
-//   if (
-//     card1.innerHTML != "&#160;" &&
-//     card2.innerHTML != "&#160;" &&
-//     card3.innerHTML != "&#160;" &&
-//     card4.innerHTML != "&#160;" &&
-//     card5.innerHTML != "&#160;" &&
-//     card6.innerHTML != "&#160;" &&
-//     card7.innerHTML != "&#160;" &&
-//     card8.innerHTML != "&#160;"
-//   ) {
-//     alert("You won!");
-//   }
-// }
+function didWin(card1, card2, card3, card4, card5, card6, card7, card8) {
+  if (
+    card1.innerHTML != "&nbsp;" &&
+    card2.innerHTML != "&nbsp;" &&
+    card3.innerHTML != "&nbsp;" &&
+    card4.innerHTML != "&nbsp;" &&
+    card5.innerHTML != "&nbsp;" &&
+    card6.innerHTML != "&nbsp;" &&
+    card7.innerHTML != "&nbsp;" &&
+    card8.innerHTML != "&nbsp;"
+  ) {
+    alert("You won!");
+  }
+}
 
 card1.addEventListener("click", onClick);
 card2.addEventListener("click", onClick);
